547a296f3ee72d48a60242b1d25f87103d272491
ThomassMathisen <thomasmathisen@hotmail.com>
2022-11-22 12:13:46 +0100

    Basic setup and connect to DB

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(console.log("Connected to MongoDB"))
+  .catch((err) => console.log(err));
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Backend is running");
+});
