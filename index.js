26ba5275ff6346573859864390ab7bd94f5f9d4c
ThomassMathisen <thomasmathisen@hotmail.com>
2022-11-22 15:42:09 +0100

    Setup auth with jwt

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const userRoutes = require("./routes/users");
+const userRoutes = require("./routes/user");
+const authRoutes = require("./routes/auth");
+const productRoutes = require("./routes/product");
+const cartRoutes = require("./routes/cart");
+const orderRoutes = require("./routes/order");
+const cors = require("cors");
 
 dotenv.config();
 
@@ -14,7 +19,13 @@ mongoose
   .then(console.log("Connected to MongoDB"))
   .catch((err) => console.log(err));
 
+app.use(cors());
+app.use(express.json());
+app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/carts", cartRoutes);
+app.use("/api/orders", orderRoutes);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Backend is running");
